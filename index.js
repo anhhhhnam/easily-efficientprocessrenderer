4e984a54ca8e56529d1e6b9a67047a2804e9196b
anhhhhnam <ryanchinespun0suy@hotmail.com>
2024-05-07 12:47:49 +0700

    update task

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-const initial = (arr) => arr.slice(0, -1);
+function findTheDifference(s, t) {
+  let result = 0;
+  for (const char of s) {
+    result ^= char.charCodeAt(0);
+  }
+  for (const char of t) {
+    result ^= char.charCodeAt(0);
+  }
+  return String.fromCharCode(result);
+}
