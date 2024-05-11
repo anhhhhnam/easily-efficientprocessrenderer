9f05aef73bcaa165c4c2469fb1ed698a7fa38d85
anhhhhnam <ryanchinespun0suy@hotmail.com>
2024-05-11 23:06:57 +0700

    feat commit

--- index.js
+++ index.js
@@ -1,17 +1,6 @@
-function countPrimes(n) {
-  const isPrime = new Array(n).fill(true);
-  isPrime[0] = false;
-  isPrime[1] = false;
-  for (let i = 2; i * i < n; i++) {
-    if (isPrime[i]) {
-      for (let j = i * i; j < n; j += i) {
-        isPrime[j] = false;
-      }
-    }
+function linearSearch(arr, target) {
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] === target) return i;
   }
-  let count = 0;
-  for (let i = 2; i < n; i++) {
-    if (isPrime[i]) count++;
-  }
-  return count;
+  return -1;
 }
