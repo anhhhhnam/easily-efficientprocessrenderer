ec268869aa09f0baf2127c66b5d0647d52bab3ca
anhhhhnam <ryanchinespun0suy@hotmail.com>
2024-05-15 04:29:05 +0700

    hello commit

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
-function moveZeroes(nums) {
-  let index = 0;
-  for (let i = 0; i < nums.length; i++) {
-    if (nums[i] !== 0) {
-      nums[index++] = nums[i];
-    }
+function isAnagram(s, t) {
+  if (s.length !== t.length) return false;
+  const map = new Map();
+  for (const char of s) {
+    map.set(char, (map.get(char) || 0) + 1);
   }
-  for (let i = index; i < nums.length; i++) {
-    nums[i] = 0;
+  for (const char of t) {
+    if (!map.has(char) || map.get(char) === 0) return false;
+    map.set(char, map.get(char) - 1);
   }
-  return nums;
+  return true;
 }
